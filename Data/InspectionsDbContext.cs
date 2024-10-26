2fff6177552dad5eaa25f43a5810ac7a78e78ad6
KENG ᅠ ᅠᅠ ᅠ <Keng7767@outlook.com>
2024-10-26 19:31:48 +0300

    Добавил DbContext

---
+++ Data/InspectionsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using RPBDISlLab4.Models;
+
+namespace RPBDISlLab4.Data
+{
+    public partial class InspectionsDbContext : DbContext
+    {
+        public InspectionsDbContext(DbContextOptions<InspectionsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Enterprise> Enterprises { get; set; }
+
+        public virtual DbSet<Inspection> Inspections { get; set; }
+
+        public virtual DbSet<Inspector> Inspectors { get; set; }
+
+
+    }
+}
